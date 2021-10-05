2569b8e2d6a07a92f4b187a8fd4faabcca31e9ca
NashTheFlash11 <71734964+NashTheFlash11@users.noreply.github.com>
2021-10-04 21:15:04 -0400

    Added firebase config

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBNRT_Iy0mHoyKqNiwXtxi6LVl-BpSXmI0",
+    authDomain: "project-67-135ca.firebaseapp.com",
+    projectId: "project-67-135ca",
+    storageBucket: "project-67-135ca.appspot.com",
+    messagingSenderId: "296080703506",
+    appId: "1:296080703506:web:71ecb33558c87e25da18cb"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
